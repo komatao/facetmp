94ba7bc995b37c333c89e3707a9bf34928abf2c7
TaoTao <tao@Komatao.local>
2020-06-28 16:37:38 +0900

    Temp

--- src/main/java/com/facetemperature/admin/config/WebConfig.java
+++ src/main/java/com/facetemperature/admin/config/WebConfig.java
@@ -21,6 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public FreeMarkerConfigurer freemarkerConfig() {
         FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+        freeMarkerConfigurer.setDefaultEncoding(Locale.getDefault(), "UTF-8");
         freeMarkerConfigurer.setTemplateLoaderPath("classpath:/templates/");
         return freeMarkerConfigurer;
     }
